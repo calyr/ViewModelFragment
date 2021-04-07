bf1b0a880684630043786470011202303e17f042
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-04-07 18:07:05 -0400

    Initial commit

---
+++ app/src/main/java/app/bo/com/ucb/viewmodelfragment/MainActivity.kt
@@ -0,0 +1,16 @@
+package app.bo.com.ucb.viewmodelfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    //lateinit var textViewMain: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      //  textViewMain = findViewById(R.id.textViewMain)
+
+      //  textViewMain
+    }
+}
---
+++ app/src/main/java/app/bo/com/ucb/viewmodelfragment/SplitFragmentOne.kt
@@ -0,0 +1,30 @@
+package app.bo.com.ucb.viewmodelfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class SplitFragmentOne : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_split_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.fragment_split_one_button).setOnClickListener {
+
+        }
+    }
+
+
+}
